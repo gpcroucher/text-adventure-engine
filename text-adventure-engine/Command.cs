6611b5b8aa13df8193b1f2162b77cdf3b33d71a3
George Croucher <gpcroucher@gmail.com>
2020-01-30 18:51:37 +0000

    moved Command to its own file

---
+++ text-adventure-engine/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_adventure_engine
+{
+    public class Command
+    {
+        public string commandName;
+        public int expectedArgs;
+        public string methodName;
+
+        public Command(string name, int args, string method)
+        {
+            commandName = name;
+            expectedArgs = args;
+            methodName = method;
+        }
+    }
+}
--- text-adventure-engine/Parser.cs
+++ text-adventure-engine/Parser.cs
@@ -149,20 +149,4 @@ namespace text_adventure_engine
 
         }
     }
-
-    public class Command
-    {
-        public string commandName;
-        public int expectedArgs;
-        public string methodName;
-
-        public Command(string name, int args, string method)
-        {
-            commandName = name;
-            expectedArgs = args;
-            methodName = method;
-        }
-    }
-
-    
 }
